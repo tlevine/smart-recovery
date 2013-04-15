e209cf694115702c8d836039566e68cb7f6a8e64
Thomas Levine <occurrence@thomaslevine.com>
2013-04-14 20:22:48 -0400

    skeletons

--- parse.py
+++ parse.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python2
-from csv import writer
 from lxml.html import parse
 
-def main():
+def table():
     html = parse('showall.php')
     trs = html.xpath('//table/tr')
-    header = clean_header(trs.pop(0))
+    header = unicode(trs.pop(0).xpath('td/descendant::text()'))
+    for tr in trs:
+        yield row(header, tr)
+
+def row(header, tr):
+    data = zip(header, map(unicode, [td.text_content() for td in tr.xpath('td')]))
 
 
-def clean_header(tr):
-    return tr.xpath('td/descendant::text()')
+def _email(tr):
+    pass
+
+def _openness(tr):
+    'Returns True, False or None'
+    pass
+
+def _location(meeting_location):
+    'Returns an address or None'
+    pass
+
+def _schedule(meeting_location):
+    'Returns (day, begin time, end time)'
+    pass
+
+def _telephone(raw):
+    pass
+
